30754e0b28c159d6c931074cfd0b8b48af87f9f9
Sansone11 <sansonemirco@gmail.com>
2022-09-05 15:41:25 +0200

    style-nav-edit

--- src/App.vue
+++ src/App.vue
@@ -21,5 +21,5 @@ export default {
 </script>
 
 <style lang="scss">
-
+  @import './components/Style/index.scss'
 </style>
--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,5 +1,25 @@
 <template>
-  <h1>Bella Zio</h1>
+  <header>
+    <div class="logo-header">
+        <img src="../assets/img/dc-logo.png" alt="">
+    </div>
+    <div>
+        <nav class="nav">
+            <ul>
+                <li class="nav_link"> <a href="#">CHARACTERS</a></li>
+                <li class="nav_link"> <a href="#">COMICS</a></li>
+                <li class="nav_link"> <a href="#">MOVIE</a></li>
+                <li class="nav_link"> <a href="#">TV</a></li>
+                <li class="nav_link"> <a href="#">GAMES</a></li>
+                <li class="nav_link"> <a href="#">COLLECTIBLES</a></li>
+                <li class="nav_link"> <a href="#">VIDEOS</a></li>
+                <li class="nav_link"> <a href="#">FANS</a></li>
+                <li class="nav_link"> <a href="#">NEWS</a></li>
+                <li class="nav_link"> <a href="#">SHOP</a></li>
+            </ul>
+        </nav>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -10,5 +30,29 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+    @import './Style/varaibles.scss';
+    header {
+        max-width: 1250px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+.nav{
+    ul{
+        display: flex;
+        align-items: center;
+        justify-content: end;
+        gap: 1rem;
 
+        .nav_link{
+            padding: 0 0.75rem;
+            line-height: 1,5rem;
+
+            &.active, &:hover{
+                background-color: $bg-active;
+                color: white;
+            }
+        }
+    }
+}
 </style>
